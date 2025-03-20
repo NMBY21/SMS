808a46b079ad54d88624781c38aac70781f5f8f5
NMBY21 <betelihemmengist21@gmail.com>
2025-03-20 13:45:46 +0300

    sms

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Http\Request;
@@ -20,7 +21,15 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
 
 // Teacher routes
 Route::middleware(['auth:sanctum', 'role:teacher'])->group(function () {
-    Route::apiResource('teacher/students', TeacherStudentController::class);
+    Route::apiResource('teacher/students', TeacherStudentController::class, [
+        'names' => [
+            'index' => 'teacher.students.index',
+            'store' => 'teacher.students.store',
+            'show' => 'teacher.students.show',
+            'update' => 'teacher.students.update',
+            'destroy' => 'teacher.students.destroy',
+        ],
+    ]);
     Route::post('teacher/students/{student}/grades', [TeacherGradeController::class, 'store']);
     Route::get('teacher/students/{student}/grades', [TeacherGradeController::class, 'index']);
 });
@@ -29,3 +38,37 @@ Route::middleware(['auth:sanctum', 'role:teacher'])->group(function () {
 Route::middleware(['auth:sanctum', 'role:student'])->group(function () {
     Route::get('student/grades', [StudentGradeController::class, 'index']);
 });
+
+
+
+// <!-- 
+
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Admin\GradeController as AdminGradeController;
+// use App\Http\Controllers\Admin\StudentController as AdminStudentController;
+// use App\Http\Controllers\Admin\SubjectController as AdminSubjectController;
+// use App\Http\Controllers\Admin\TeacherController as AdminTeacherController;
+// use App\Http\Controllers\Teacher\GradeController as TeacherGradeController;
+// use App\Http\Controllers\Teacher\StudentController as TeacherStudentController;
+// use App\Http\Controllers\Student\GradeController as StudentGradeController;
+
+// // Admin routes
+// Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+//     Route::apiResource('admin/teachers', AdminTeacherController::class);
+//     Route::apiResource('admin/students', AdminStudentController::class);
+//     Route::apiResource('admin/subjects', AdminSubjectController::class);
+//     Route::apiResource('admin/grades', AdminGradeController::class);
+// });
+
+// // Teacher routes
+// Route::middleware(['auth:sanctum', 'role:teacher'])->group(function () {
+//     Route::apiResource('teacher/students', TeacherStudentController::class);
+//     Route::post('teacher/students/{student}/grades', [TeacherGradeController::class, 'store']);
+//     Route::get('teacher/students/{student}/grades', [TeacherGradeController::class, 'index']);
+// });
+
+// // Student routes
+// Route::middleware(['auth:sanctum', 'role:student'])->group(function () {
+//     Route::get('student/grades', [StudentGradeController::class, 'index']);
+// }); -->
